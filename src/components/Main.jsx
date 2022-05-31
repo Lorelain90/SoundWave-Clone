43fb0e20c7cf55c0db44dc3de3a4f278a3b0eacf
Lorelain <baldomerland@gmail.com>
2022-05-31 14:15:19 +0200

    Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Discover from "./Discover";
 import Girl from "./images/landing-page-girl.png";
 
 const Main = () => {
   return (
-    <>
+    <div className="main">
         
       <img src={Girl} alt="Girl" />
       <h1>Feel The Music</h1>
       <p>Stream over 20 thousand song with one</p>
       <p>click</p>
-      <Button>Join Now</Button>
-    </>
+      <Button a href="#Join">Join Now</Button>
+    </div>
   );
 };
 
